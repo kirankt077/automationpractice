25be22f47e9dc06fd7b52c7aafda6d5ab6f0926c
Kiran Thakur <57950467+kirankt077@users.noreply.github.com>
2021-09-20 13:25:53 +0530

    Update SocialMedia_Tests.java

--- automationpractice_Java/src/test/java/tests/WebAPP/SocialMedia_Tests.java
+++ automationpractice_Java/src/test/java/tests/WebAPP/SocialMedia_Tests.java
@@ -9,12 +9,6 @@ import io.qameta.allure.*;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-/**
- * Test_Automation-automationpractice
- *
- * @author kamil.nowocin
- **/
-
 @Epic("Web App Tests")
 @Feature("SOCIAL MEDIA TESTS")
 @Listeners({TestNGListener.class})
